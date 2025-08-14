86eded670a2f14243f784114d331d4d84a07c6a8
rishit khandelwal <rishitkh@gmail.com>
2025-08-15 01:53:39 +0530

    basic challenge

---
+++ app/api/submit/route.ts
@@ -0,0 +1,29 @@
+import {NextResponse} from 'next/server';
+import bcrypt from 'bcrypt'
+require('dotenv').config()
+
+const salt = "lmaolmaolmaolmaolmaolmaolmaolmaolmaolmaolmaolmaolmaolmaolmaolmaolma";
+const FLAG = process.env.FLAG
+
+export async function POST(request: Request) {
+  const formData = await request.formData();
+  const username = formData.get('username');
+  const password = formData.get('password');
+
+  if (username == 'admin') {
+    console.log(bcrypt.hashSync(username+salt+password, 12))
+    return NextResponse.json({
+      message: "LOGIN AS ADMIN NOT ALLOWED"
+    })
+  }
+
+  if (await bcrypt.compare(username+salt+password, '$2b$12$Ga1UIWfKFSjYKB0kmJK88eEj8V/rbDHlGCIGfIq3S3uZdYoqkchFe')) {
+    return NextResponse.json({
+      message: `the flag is ${FLAG}`
+    })
+  }
+  
+  return NextResponse.json({
+    message: `hello newb`
+  })
+}
